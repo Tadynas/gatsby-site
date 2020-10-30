264868b40a6a0a554e4fff2a1990fcb1dbb2cc39
Tadas Gricius <griciustadas@gmail.com>
2020-10-30 11:43:45 +0200

    Linking between pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <div>
       <h1>About</h1>
       <p>This is my about page.</p>
+      <p><Link to="/contact">Contact me.</Link></p>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 const ContactPage = () => {
   return (
     <div>
       <h1>Contact</h1>
       <p>This is my contact page.</p>
+      <a href="https://www.linkedin.com" target="_blank">My LinkedIn</a>
     </div>
   )
 }
