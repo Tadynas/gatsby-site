5aed6279a0a1a4d94c0a8fbf95e1197965707a32
Tadas Gricius <griciustadas@gmail.com>
2020-10-30 12:12:49 +0200

    Shared components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
+import Header from '../components/header'
+import Footer from '../components/footer'
+
 const ContactPage = () => {
   return (
     <div>
+      <Header />
       <h1>Contact</h1>
       <p>This is my contact page.</p>
-      <a href="https://www.linkedin.com" target="_blank">My LinkedIn</a>
+      <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">My LinkedIn</a>
+      <Footer />
     </div>
   )
 }
 
-export default ContactPage
+export default ContactPage 
